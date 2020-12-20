52ff6342db359fcb9fefb39875de0e58093e6a46
oscar <os.quezadar@alumnos.cuoc.cl>
2020-12-20 20:30:17 -0300

    detalles del footer arreglados

--- ventas/urls.py
+++ ventas/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.principal, name='principal'),
-    path('formulario',views.post_new,name='formulario')
+    path('formulario',views.post_new,name='formulario'),
     
 ]
